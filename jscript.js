3f4253ee889ed2964bff0ca24849d86a07e2ac8c
João Vítor Fonseca Silva <92068756+jaumziin@users.noreply.github.com>
2023-02-13 11:58:21 -0300

    feat - Adicionando arquivo JS e primeiros códigos
    Como já explícito no título, adicionei um arquivo *javascript* e codei os primeiros códigos, já inicializando com desejo de aprender JavaScript

---
+++ jscript.js
@@ -0,0 +1,8 @@
+console.log("Olá Mundo!");
+
+var message = ("Você está preparado para apanhar?!")
+alert(message);
+
+var title = document.querySelector(".menu-title");
+
+title.textContent = "Com o 'Combo+' você e toda sua família será mais feliz!"; 
